5672a97b8116a215ef548e696ca65eb4f617867c
Ricky Renaldi <rickyrenaldi100@gmail.com>
2024-04-03 07:50:26 +0700

    menambahkan class DataMahasiswa1302223051

--- modul7_kelompok_2/Program.cs
+++ modul7_kelompok_2/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class main
+{
+    public static void Main(string[] args)
+    {
+        DataMahasiswa1302223051 mahasiswa1302223051 = new DataMahasiswa1302223051();
+
+        mahasiswa1302223051.ReadJSON();
+    }
+}
